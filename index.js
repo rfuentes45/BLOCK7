d912dfd295b6b93c4a3614e1b11542bdd86bd148
ricardo fuentes <richard45oen@gmail.com>
2024-01-19 13:57:51 -0600

    added block 7

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+//we make messsage about vault
+
+
+
+const introMessage='You have received this message because you have been chosen to open an important vault. Here is the secret combination '
+
+//these are the eqautions to get codes
+const num1= 2*5
+const num2= 8+(8*4)
+const num3= 289 % 50
+
+
+//we are outputing the numbers with console log
+console.log('Number 1:', num1)
+console.log('Number 2:', num2)
+console.log('Number 3:', num3)
+
+
+//add dialog box for message
+//this is the alert popup with message and numbers evenly spaced out
+alert(introMessage + '\n' + num1 + '-' + num2 + '-' +num3)
